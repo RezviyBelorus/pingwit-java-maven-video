43edfce12241652c761705357266536cb320abb4
Alexander Fomin <rezviybelorus@gmail.com>
2024-03-11 22:38:17 +0100

    part_29 json array read/write

--- src/main/java/com/pingwit/part_29/read/JacksonJsonWriterExample.java
+++ src/main/java/com/pingwit/part_29/read/JacksonJsonWriterExample.java
@@ -3,10 +3,11 @@ package com.pingwit.part_29.read;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 
-public class JacksonJsonReaderExample {
+public class JacksonJsonWriterExample {
     public static void main(String[] args) throws IOException {
         ObjectMapper mapper = new ObjectMapper();
         mapper.registerModule(new JavaTimeModule());
@@ -16,7 +17,7 @@ public class JacksonJsonReaderExample {
         User user = mapper.readValue(jsonFileStream, User.class);
         System.out.println(user);
 
-        String json = mapper.writeValueAsString(user);
-        System.out.println(json);
+        File output = new File("src/main/java/com/pingwit/part_29/read/user.json");
+        mapper.writeValue(output, user);
     }
 }
--- src/main/java/com/pingwit/part_29/read/User.java
+++ src/main/java/com/pingwit/part_29/read/User.java
@@ -1,10 +1,13 @@
 package com.pingwit.part_29.read;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.time.LocalDate;
 import java.util.List;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 record User(
         String username,
         String email,
@@ -15,6 +18,8 @@ record User(
         String gender,
         List<String> interests,
         Location location,
-        String profilePicture
+        String profilePicture,
+        @JsonProperty("premium_access")
+        Boolean premiumAccess
 ) {
 }
